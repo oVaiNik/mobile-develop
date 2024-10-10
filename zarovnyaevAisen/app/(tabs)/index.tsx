67cdb15b895dbb9e06b7187b230ff5643167302d
zuckr7 <realslimshady30@gmail.com>
2024-10-10 19:43:00 +0900

    lease enter the commit message for your changes. Lines trig# with '#' will be ignored, and an empty message aborts the commi.#

--- zarovnyaevAisen/app/(tabs)/index.tsx
+++ zarovnyaevAisen/app/(tabs)/index.tsx
@@ -1,24 +1,37 @@
-import { Image, StyleSheet, Platform, SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
-import React, { useState } from 'react';
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import {
+  Image,
+  StyleSheet,
+  Platform,
+  SafeAreaView,
+  View,
+  Text,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { HelloWave } from "@/components/HelloWave";
+import ParallaxScrollView from "@/components/ParallaxScrollView";
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
 
-
-const colors : string[] = ["black","red","blue","green","yellow","purple"];
-const colorsText: string[] = ["Цвет квадрата черный","Цвет квадрата красный","Цвет квадрата синий","Цвет квадрата зелёный","Цвет квадрата жёлтый","Цвет квадрата фиолетовый"]
+const colors: string[] = ["black", "red", "blue", "green", "yellow", "purple"];
+const colorsText: string[] = [
+  "Цвет квадрата черный",
+  "Цвет квадрата красный",
+  "Цвет квадрата синий",
+  "Цвет квадрата зелёный",
+  "Цвет квадрата жёлтый",
+  "Цвет квадрата фиолетовый",
+];
 export default function HomeScreen() {
-
   const [colorIndex, setColorIndex] = useState(0); // Меняет цвет
   const [colorChangeCount, setColorChangeCount] = useState(0); // Счётчик кликов на квадрат
   const [displayText, setDisplayText] = useState(colorsText[0]); // Говорит какого цвета квадрат
   return (
     <SafeAreaView style={{ flex: 1, display: "flex" }}>
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Привет мир</Text>
+        <Text>Привет мир</Text>
 
-      <Text style={{ marginTop: 20 }}>{displayText}</Text>
+        <Text style={{ marginTop: 20 }}>{displayText}</Text>
         <View
           style={{
             backgroundColor: colors[colorIndex],
@@ -42,7 +55,10 @@ export default function HomeScreen() {
         >
           <Text style={{ color: "white" }}>Не нажимай!</Text>
         </TouchableOpacity>
-        <Text style={{ marginTop: 20 }}> Цвет поменялся: {colorChangeCount} раз </Text>
+        <Text style={{ marginTop: 20 }}>
+          {" "}
+          Цвет поменялся: {colorChangeCount} раз{" "}
+        </Text>
       </View>
     </SafeAreaView>
   );
@@ -50,8 +66,8 @@ export default function HomeScreen() {
 
 const styles = StyleSheet.create({
   titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     gap: 8,
   },
   stepContainer: {
@@ -63,6 +79,6 @@ const styles = StyleSheet.create({
     width: 290,
     bottom: 0,
     left: 0,
-    position: 'absolute',
+    position: "absolute",
   },
 });
