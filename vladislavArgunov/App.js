22e60806f5a895c76a38e33c2ab0fd5f1ec9314e
Kawasaki4112 <90658819+kawasaki4112@users.noreply.github.com>
2024-10-10 19:22:23 +0900

    useState

--- vladislavArgunov/App.js
+++ vladislavArgunov/App.js
@@ -1,20 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, TextInput, Button, Text, View } from 'react-native';
 
-export default function App() {
+export default function App() 
+{
+  const [number, setNumber] = useState(0);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.defaultStyle}>
+        {number}
+      </Text>
+      <Text style={styles.defaultStyle}>
+        Нажимай
+      </Text>
+      <Button 
+        title='на меня' 
+        onPress={()=> setNumber(number+1)}
+      />
+      <Button 
+        title='не сюда' 
+        onPress={()=> setNumber(number = -9999)}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+const styles = StyleSheet.create(
+{
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  defaultStyle: 
+  {
+    fontSize: 20,
+    marginBottom: 40,
+  }
 });
