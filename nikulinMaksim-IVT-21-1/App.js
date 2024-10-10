35b1e25bdc074620716e240d7747dcaf919b2e71
Nikulin Maksim <maksim.nikulin2017@mail.ru>
2024-10-10 18:03:10 +0900

    TouchableOpacity 1

--- nikulinMaksim-IVT-21-1/App.js
+++ nikulinMaksim-IVT-21-1/App.js
@@ -1,20 +1,52 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  const [count, setCount] = useState(0);
+  const onPress = () => setCount(prevCount => prevCount + 1);
+
+  const colors = ["red", "green", "blue"]
+  const [colorIndex, setColorIndex] = useState(0)
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View>
+        <View style={styles.countContainer}>
+          <Text>Count: {count}</Text>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={onPress}>
+          <Text>Press Here</Text>
+        </TouchableOpacity>
+      </View>
+
+      <View style={styles.boxContainer}>
+        <TouchableOpacity onPress={() => {
+          setColorIndex((colorIndex + 1) % 3)
+        }}>
+          <View style={{height: 100, width: 100, backgroundColor: colors[colorIndex]}}></View>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 10,
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+  },
+  countContainer: {
+    alignItems: 'center',
+    padding: 10,
+  },
+  boxContainer: {
+    alignItems: "center",
+    marginTop: 20,
   },
 });
