e161299f2777882d374c6e60977eeca926d6e0c3
Kawasaki4112 <90658819+kawasaki4112@users.noreply.github.com>
2024-10-10 19:44:29 +0900

    prettier

--- vladislavArgunov/App.js
+++ vladislavArgunov/App.js
@@ -1,41 +1,28 @@
-import { useState } from 'react';
-import { StyleSheet, TextInput, Button, Text, View } from 'react-native';
+import { useState } from "react";
+import { StyleSheet, TextInput, Button, Text, View } from "react-native";
 
-export default function App() 
-{
+export default function App() {
   const [number, setNumber] = useState(0);
 
   return (
     <View style={styles.container}>
-      <Text style={styles.defaultStyle}>
-        {number}
-      </Text>
-      <Text style={styles.defaultStyle}>
-        Нажимай
-      </Text>
-      <Button 
-        title='на меня' 
-        onPress={()=> setNumber(number+1)}
-      />
-      <Button 
-        title='не сюда' 
-        onPress={()=> setNumber(-9999)}
-      />
+      <Text style={styles.defaultStyle}>{number}</Text>
+      <Text style={styles.defaultStyle}>Нажимай</Text>
+      <Button title="на меня" onPress={() => setNumber(number + 1)} />
+      <Button title="не сюда" onPress={() => setNumber(-9999)} />
     </View>
   );
 }
 
-const styles = StyleSheet.create(
-{
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
-  defaultStyle: 
-  {
+  defaultStyle: {
     fontSize: 20,
-    marginBottom: 40,
-  }
+    marginBottom: 20,
+  },
 });
