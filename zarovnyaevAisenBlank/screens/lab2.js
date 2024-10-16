3357ed63f225d0a8c426b1c32888198b2fca5898
zuckr7 <realslimshady30@gmail.com>
2024-10-17 04:21:52 +0900

    prettier

--- zarovnyaevAisenBlank/screens/lab2.js
+++ zarovnyaevAisenBlank/screens/lab2.js
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, View, Text, Button, ScrollView } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { SafeAreaView, View, Text, Button, ScrollView } from "react-native";
 
 const countries = [
-  { code: 'AT', name: 'Австрия' },
-  { code: 'US', name: 'США' },
-  { code: 'CA', name: 'Канада' },
-  { code: 'FR', name: 'Франция' },
-  { code: 'GB', name: 'Великобритания' },
-  { code: 'JP', name: 'Япония' },
-  { code: 'RU', name: 'Россия' },
+  { code: "AT", name: "Австрия" },
+  { code: "US", name: "США" },
+  { code: "CA", name: "Канада" },
+  { code: "FR", name: "Франция" },
+  { code: "GB", name: "Великобритания" },
+  { code: "JP", name: "Япония" },
+  { code: "RU", name: "Россия" },
 ];
 
 export default function AdministrativeDivisionsScreen() {
@@ -17,12 +17,14 @@ export default function AdministrativeDivisionsScreen() {
 
   const fetchDivisions = async (countryCode) => {
     try {
-      const response = await fetch(`https://rawcdn.githack.com/kamikazechaser/administrative-divisions-db/master/api/${countryCode}.json`);
+      const response = await fetch(
+        `https://rawcdn.githack.com/kamikazechaser/administrative-divisions-db/master/api/${countryCode}.json`
+      );
       const data = await response.json();
-      console.log('Fetched Data:', data);
+      console.log("Fetched Data:", data);
       setDivisions(data); // Устанавливаем данные административных единиц
     } catch (error) {
-      console.error('Error fetching divisions:', error);
+      console.error("Error fetching divisions:", error);
     }
   };
 
@@ -36,9 +38,24 @@ export default function AdministrativeDivisionsScreen() {
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ScrollView contentContainerStyle={{ flexGrow: 1, justifyContent: "center", alignItems: "center" }}>
-        <View style={{ margin: 20, flex: 1, justifyContent: "center", alignItems: "center" }}>
-          <Text style={{ marginBottom: 10 }}>Административные единицы в {countries[countryIndex].name}:</Text>
+      <ScrollView
+        contentContainerStyle={{
+          flexGrow: 1,
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <View
+          style={{
+            margin: 20,
+            flex: 1,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Text style={{ marginBottom: 10 }}>
+            Административные единицы в {countries[countryIndex].name}:
+          </Text>
           {divisions.map((division, index) => (
             <Text key={index}>{division}</Text> // Отображаем каждую административную единицу
           ))}
