8362fdc6185270422d099f5ec7d4a77c121ea11b
Peter Bols <bolshakov_pv@albank.ru>
2024-10-16 17:05:32 +0900

    ne dodelano

--- bolshakovPetr/app/(tabs)/index.tsx
+++ bolshakovPetr/app/(tabs)/index.tsx
@@ -1,81 +1,97 @@
 import { Image, StyleSheet, Platform, SafeAreaView, View, Text, TouchableOpacity } from 'react-native';
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import axios from "axios"
 
+const colors : string[] = ["black","red","blue","green","yellow","purple"];
+const colorsText: string[] = ["Черный","Красный","Синий","Зелёный","Жёлтый","Фиолетовый"]
 export default function HomeScreen() {
-    const [characters, setCharacters] = useState([]);
-    const [nextChar, setNextChar] = useState(1);
 
-    useEffect(() => {
-        axios.get('https://rickandmortyapi.com/api/character')
-            .then(res => {
-                setCharacters(res.data.results)
-                console.log(characters)
-            })
-        }, [])
+const [colorIndex, setColorIndex] = useState(0);
+const [boxSize, setBoxSize] = useState(100);
 
-    function NextFunc(val){
-        if (val > 19){
-            setNextChar(1)
-        }
-        else{
-            setNextChar(nextChar + 1)
-        }
-    }
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1)) + min;
 
-    function PrevFunc(val){
-        if (val == 1){
-            setNextChar(20)
-        }
-        else{
-            setNextChar(nextChar - 1)
-        }
-    }
+const [displayText, setDisplayText] = useState(colorsText[0]);
 
-    return (
-        <SafeAreaView style={{ flex: 1, display: "flex" }}>
-        <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+return (
+    <SafeAreaView style={{ flex: 1, display: "flex" }}>
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <View
+    style={{
+        backgroundColor: colors[colorIndex],
+        width: boxSize,
+        height: boxSize,
+        }}
+    />
+    <TouchableOpacity
+    onPress={() => {
+        setColorIndex((colorIndex + 1) % colors.length);
+        setDisplayText(colorsText[(colorIndex + 1) % colors.length]);
+        }}
+    style={{
+        backgroundColor: "green",
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 10,
+        }}
+    >
+    <Text style={{ color: "white" }}>Поменяй цвет</Text>
+    </TouchableOpacity>
+    <TouchableOpacity
+    onPress={() => {
+        setBoxSize(randomInt(100, 150));
+        }}
+    style={{
+        backgroundColor: "yellow",
+        padding: 10,
+        borderRadius: 10,
+        marginTop: 10,
+        }}
+     >
+     <Text style={{ color: "white" }}>Поменяй размер</Text>
+     </TouchableOpacity>
+     <Text style={{ marginTop: 20 }}>Текущий цвет - {displayText}</Text>
 
-        <Text style={{ marginBottom: 20 }}>{characters[nextChar-1].name}</Text>
-        <img
-            src = {characters[nextChar-1].image}
-        />
-
-        <TouchableOpacity
-        onPress={() => {
-            PrevFunc(nextChar)
-            }}
-        style={{
-            backgroundColor: "green",
-            padding: 10,
-            borderRadius: 10,
-            marginTop: 10,
-            }}
-        >
-
-        <Text style={{ color: "white" }}>Предыдущий</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-        onPress={() => {
-            NextFunc(nextChar)
-            }
-        }
-        style={{
-            backgroundColor: "green",
-            padding: 10,
-            borderRadius: 10,
-            marginTop: 10,
-            }}
-        >
-        <Text style={{ color: "white" }}>Следующий</Text>
-        </TouchableOpacity>
-        <Text style={{ marginTop: 20 }}>Текущий цвет - {nextChar}</Text>
-        <Text style={{ marginTop: 20 }}>Текущий цвет - {characters[nextChar-1].id}</Text>
-        </View>
-        </SafeAreaView>
+//          <Text style={{ marginBottom: 20 }}>{characters[nextChar-1].name}</Text>
+//              <img
+//                 src = {characters[nextChar-1].image}
+//             />
+//
+//             <TouchableOpacity
+//             onPress={() => {
+//                 PrevFunc(nextChar)
+//                 }}
+//             style={{
+//                 backgroundColor: "green",
+//                 padding: 10,
+//                 borderRadius: 10,
+//                 marginTop: 10,
+//                 }}
+//             >
+//
+//             <Text style={{ color: "white" }}>Предыдущий</Text>
+//             </TouchableOpacity>
+//             <TouchableOpacity
+//             onPress={() => {
+//                 NextFunc(nextChar)
+//                 }
+//             }
+//             style={{
+//                 backgroundColor: "green",
+//                 padding: 10,
+//                 borderRadius: 10,
+//                 marginTop: 10,
+//                 }}
+//             >
+//             <Text style={{ color: "white" }}>Следующий</Text>
+//             </TouchableOpacity>
+//             <Text style={{ marginTop: 20 }}>Текущий цвет - {nextChar}</Text>
+//             <Text style={{ marginTop: 20 }}>Текущий цвет - {characters[nextChar-1].id}</Text>
+    </View>
+    </SafeAreaView>
     );
 }
