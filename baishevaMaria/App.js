cd418a24697dab1afd831f7b6a058c2e01276eae
MariaBaisheva <mariabaisevp22001@gmail.com>
2024-10-23 16:48:26 +0900

    lab3

--- baishevaMaria/App.js
+++ baishevaMaria/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View, Button } from 'react-native';
 import React, { useState } from 'react';
 import Lab1 from "./screens/lab1.js";
 import Lab2 from "./screens/lab2.js";
+import Lab3 from './screens/lab3.js';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 
@@ -14,6 +15,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Lab1" component={Lab1} />
         <Tab.Screen name="Lab2" component={Lab2} />
+        <Tab.Screen name="Lab3" component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- baishevaMaria/screens/lab1.js
+++ baishevaMaria/screens/lab1.js
@@ -5,7 +5,7 @@ export default function Lab1() {
   const [message, setMessage] = useState('Нажмите кнопку');
   const [bgColor, setBgColor] = useState('#fff');
   const changeBackgroundColor = () => {
-    setMessage('Кнопка нажата!');
+    setMessage('Нажми на кнопку!');
     const colors = ['#FF5733', '#33FF57', '#3357FF', '#FFFF33', '#FF33FF'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     setBgColor(randomColor);
