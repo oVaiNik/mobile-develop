0e8e7fa790614eebb1af79c47e560fe3c1499b02
scmbr <145434167+scmbr@users.noreply.github.com>
2024-10-24 20:14:59 +0900

    добавил лаб3

--- romanovVasiliy/App.js
+++ romanovVasiliy/App.js
@@ -1,6 +1,7 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Lab1 from "./screens/lab1";
 import Lab2 from "./screens/lab2";
+import Lab3 from "./screens/lab3";
 import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
@@ -10,6 +11,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="Лабораторная 1" component={Lab1} />
         <Tab.Screen name="Лабораторная 2" component={Lab2} />
+        <Tab.Screen name="Лабораторная 3" component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
