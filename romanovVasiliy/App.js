48c9c81ffd318900193f49fe52a7ced8161cfb62
scmbr <145434167+scmbr@users.noreply.github.com>
2024-10-24 19:47:23 +0900

    сделал лаб1

--- romanovVasiliy/App.js
+++ romanovVasiliy/App.js
@@ -1,7 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Lab1 from "./screens/lab1";
-import Lab2 from "./screens/lab2";
-import Lab3 from "./screens/lab3";
 import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
@@ -10,8 +8,6 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Лабораторная 1" component={Lab1} />
-        <Tab.Screen name="Лабораторная 2" component={Lab2} />
-        <Tab.Screen name="Лабораторная 3" component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
