4815b8477311dbb9566a740f71d4923f25d9e5ea
scmbr <145434167+scmbr@users.noreply.github.com>
2024-10-24 20:02:39 +0900

    удалил лаб2 пока что

--- romanovVasiliy/App.js
+++ romanovVasiliy/App.js
@@ -1,6 +1,5 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Lab1 from "./screens/lab1";
-import Lab2 from "./screens/lab2";
 import { NavigationContainer } from "@react-navigation/native";
 
 const Tab = createBottomTabNavigator();
@@ -9,7 +8,6 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Лабораторная 1" component={Lab1} />
-        <Tab.Screen name="Лабораторная 2" component={Lab2} />
       </Tab.Navigator>
     </NavigationContainer>
   );
