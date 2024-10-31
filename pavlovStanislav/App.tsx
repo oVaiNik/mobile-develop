070836c22d5a55429c415cac5ad1f7f7cb652458
Павлов Станислав Степанович <pavlov_ss@albank.ru>
2024-10-31 15:02:59 +0900

    Add React navigation

--- pavlovStanislav/App.tsx
+++ pavlovStanislav/App.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import Lab1 from './src/screens/Lab1'
+import {NavigationContainer} from '@react-navigation/native'
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs'
+
+const Tab = createBottomTabNavigator()
 
 const App: React.FC = () => {
-  return <Lab1 />
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Lab1" component={Lab1} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 export default App
