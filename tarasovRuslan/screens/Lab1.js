109c252f5e1e269fd5ae09304aa86d27cae4a282
Ruslan32145 <tarasovr429@gmail.com>
2024-11-11 00:38:53 +0900

    Перешел на template blank + lab1

--- tarasovRuslan/screens/Lab1.js
+++ tarasovRuslan/screens/Lab1.js
@@ -1,23 +1,42 @@
 import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { useState } from "react";
+
 const colors = ["black", "red", "yellow"];
+const shapes = ["square", "circle", "triangle"];
 
 const Lab1 = () => {
   const [colorIndex, setColorIndex] = useState(0);
+  const [shapeIndex, setShapeIndex] = useState(0);
+
+  const shapeStyle = {
+    backgroundColor: colors[colorIndex],
+    width: 100,
+    height: 100,
+    ...(shapes[shapeIndex] === "circle" && {
+      borderRadius: 50,
+    }),
+    ...(shapes[shapeIndex] === "triangle" && {
+      width: 0,
+      height: 0,
+      backgroundColor: "transparent",
+      borderLeftWidth: 50,
+      borderRightWidth: 50,
+      borderBottomWidth: 100,
+      borderLeftColor: "transparent",
+      borderRightColor: "transparent",
+      borderBottomColor: colors[colorIndex],
+    }),
+  };
+
   return (
     <SafeAreaView style={{ flex: 1, display: "flex" }}>
-      <Text>hello</Text>
+      <Text>Первая лабораторная работа.</Text>
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <View
-          style={{
-            backgroundColor: colors[colorIndex],
-            width: 100,
-            height: 100,
-          }}
-        />
+        <View style={shapeStyle} />
         <TouchableOpacity
           onPress={() => {
-            setColorIndex((colorIndex + 1) % 3);
+            setColorIndex((colorIndex + 1) % colors.length);
+            setShapeIndex((shapeIndex + 1) % shapes.length);
           }}
           style={{
             backgroundColor: "green",
@@ -26,7 +45,7 @@ const Lab1 = () => {
             marginTop: 10,
           }}
         >
-          <Text style={{ color: "white" }}>Press me!</Text>
+          <Text style={{ color: "white" }}>Измени мой цвет и фигуру!</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
