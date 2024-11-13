d4ddcf38689de9a0c55ad67ebc28fa7fa476ddf3
Ruslan32145 <tarasovr429@gmail.com>
2024-11-14 01:07:01 +0900

    Лабораторная 2

--- tarasovRuslan/App.js
+++ tarasovRuslan/App.js
@@ -2,6 +2,7 @@ import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
 import { useState } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Lab1 from "./screens/Lab1";
+import Lab2 from "./screens/Lab2";
 import { NavigationContainer } from "@react-navigation/native";
 const colors = ["black", "red", "yellow"];
 
@@ -11,6 +12,7 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Lab1" component={Lab1} />
+        <Tab.Screen name="Lab2" component={Lab2} />
       </Tab.Navigator>
     </NavigationContainer>
   );
