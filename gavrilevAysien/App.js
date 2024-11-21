345065e5f86dc006a5e3d97bcd71c0849980a33b
AG1CE <144991640+AG1CE@users.noreply.github.com>
2024-11-21 19:43:59 +0900

    Lab1
    Создал кнопку изменяющую цвет круга по центру экрана

--- gavrilevAysien/App.js
+++ gavrilevAysien/App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, Text, TouchableOpacity, View } from "react-native";
+import { useState } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Lab1 from "./screens/lab1";
+import { NavigationContainer } from "@react-navigation/native";
+const colors = ["black", "red", "blue"];
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Lab1" component={Lab1} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
