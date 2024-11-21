145eec559b5978df3f1c2ed3d71d94e80bb145c0
Malyshkavlad <realforgotyou2002@mail.ru>
2024-11-21 20:00:33 +0900

    lab1

--- vladMalyshev/App.js
+++ vladMalyshev/App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import lab1 from "./screens/lab1";
+import { NavigationContainer } from "@react-navigation/native";
 
+const Tab = createBottomTabNavigator();
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Лабораторная 1" component={lab1} />
+      </Tab.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
