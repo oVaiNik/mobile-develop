200ef28441d388d76441c1cd9b663d6f368b00a1
Inna <90662377+innk88@users.noreply.github.com>
2024-11-27 20:36:38 +0900

    lab3
    вычисляется факториал числа, usememo используется, чтобы факториал вычислялся только при изменении входного числа

--- innaKulichkina-IVT-21-2/innaKulichkina/App.js
+++ innaKulichkina-IVT-21-2/innaKulichkina/App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ColorAndMoveScreen from './screens/lab1'; 
 import CatFactApp from './screens/lab2'; 
+import Lab3 from './screens/lab3';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,6 +13,7 @@ export default function App() {
       <Tab.Navigator>
         <Tab.Screen name="№1" component={ColorAndMoveScreen} />
         <Tab.Screen name="№2" component={CatFactApp} />
+        <Tab.Screen name="№3" component={Lab3} />
       </Tab.Navigator>
     </NavigationContainer>
   );
