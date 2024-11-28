07b51bcaade0c3b46bac47832354997f4f97f31e
Your Name <you@example.com>
2024-11-29 00:06:55 +0900

    анимация

--- arsenNikiforovCLI/App.js
+++ arsenNikiforovCLI/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Animated} from 'react-native'; 
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
