9c0e160e517a4286231682a3b47182abd312d028
Arghost2003 <arilin200009@gmail.com>
2024-12-16 20:27:23 +0900

    Lab 3-4
    Сделал лабораторные работы 3-4

--- arsenIlin/App.js
+++ arsenIlin/App.js
@@ -3,6 +3,8 @@ import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 const Tab = createBottomTabNavigator();
 
 import Lab2 from "./layouts/lab2";
+import Lab3 from "./layouts/lab3";
+import Lab4 from "./layouts/lab4";
 import { NavigationContainer } from "@react-navigation/native";
 const colors = ["black", "red", "yellow"];
 
@@ -11,6 +13,8 @@ export default function App() {
     <NavigationContainer>
       <Tab.Navigator>
         <Tab.Screen name="Lab2" component={Lab2} />
+        <Tab.Screen name="Lab3" component={Lab3} />
+        <Tab.Screen name="Lab4" component={Lab4} />
       </Tab.Navigator>
     </NavigationContainer>
   );
--- potapovAndrey/lab1/lab1/app/index.tsx
+++ potapovAndrey/lab1/lab1/app/index.tsx
@@ -1,8 +1,6 @@
 import { useState } from "react";
 import { Text } from "react-native";
 import Lab1 from "./lab1";
-import Lab2 from "./lab2";
-import Lab3 from "./lab3";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
@@ -14,8 +12,6 @@ export default function Index() {
   return (
     <Tab.Navigator>
       <Tab.Screen name="Лаба 1" component={Lab1} />
-      <Tab.Screen name="Лаба 2" component={Lab2} />
-      <Tab.Screen name="Лаба 3" component={Lab3} />
     </Tab.Navigator>
   );
 }
