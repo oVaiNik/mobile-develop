8bb06c8a4d8580e7d90a3f4f348d12c7ba867a41
Nurgun2003 <90663118+Nurgun2003@users.noreply.github.com>
2024-12-19 20:20:11 +0900

    Prettier Lab4

--- neustroevNurgun/store/TextStore.js
+++ neustroevNurgun/store/TextStore.js
@@ -1,24 +1,24 @@
 import { createContext, useContext } from "react";
-import { observer, useLocalObservable } from 'mobx-react-lite';
+import { observer, useLocalObservable } from "mobx-react-lite";
 
 const Context = createContext(null);
 
 export const TextStoreProvider = observer(({ children, ...props }) => {
-	const store = useLocalObservable(() => createTextStore(props));
-	return <Context.Provider value={store}>{children}</Context.Provider>;
+  const store = useLocalObservable(() => createTextStore(props));
+  return <Context.Provider value={store}>{children}</Context.Provider>;
 });
 
 export const useTextStore = () => {
-	const store = useContext(Context);
-	if(!store) throw new Error('Используйте TextStore внутри провайдера!');
-	return store;
-}
+  const store = useContext(Context);
+  if (!store) throw new Error("Используйте TextStore внутри провайдера!");
+  return store;
+};
 
 export const createTextStore = (props) => {
-	return {
-		text: '',
-		setText(newText) {
-			this.text = newText;
-		}
-	};
+  return {
+    text: "",
+    setText(newText) {
+      this.text = newText;
+    },
+  };
 };
