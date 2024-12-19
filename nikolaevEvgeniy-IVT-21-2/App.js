3e8e9c28ef438ad3d535f734b41113136251b43e
L1ghtowo2803 <90662386+L1ghtowo2803@users.noreply.github.com>
2024-12-19 20:40:52 +0900

    lab3

--- nikolaevEvgeniy-IVT-21-2/App.js
+++ nikolaevEvgeniy-IVT-21-2/App.js
@@ -5,6 +5,8 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 import Lab1 from "./Screens/Lab1";
 import Lab2 from "./Screens/Lab2";
+import Lab3 from "./Screens/Lab3";
+import TaskList from "./Screens/TaskList";
 
 const Tab = createBottomTabNavigator();
 
@@ -15,6 +17,8 @@ export default function App() {
         <Tab.Navigator>
           <Tab.Screen name="Lab1" component={Lab1} />
           <Tab.Screen name="Lab2" component={Lab2} />
+          <Tab.Screen name="Lab3" component={Lab3} />
+          <Tab.Screen name="Tasks" component={TaskList} />
         </Tab.Navigator>
       </NavigationContainer>
     </Provider>
