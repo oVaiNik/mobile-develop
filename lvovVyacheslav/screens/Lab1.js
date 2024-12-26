37281b8cc14a364e86fffb781e8978c4ea9397b3
Sivoziche <118076242+Sivoziche@users.noreply.github.com>
2024-12-26 20:07:04 +0900

    2lab, useState
    Львов Вячеслав

--- lvovVyacheslav/screens/Lab1.js
+++ lvovVyacheslav/screens/Lab1.js
@@ -2,28 +2,53 @@ import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import React, { useState } from 'react';
 
 const Lab1 = () => {
-  const [count, setCount] = useState(0);
+  // Локальное состояние для хранения количества нажатий
+  const [clicks, setClicks] = useState(0);
+
+  // Функция для увеличения счетчика
+  const incrementClicks = () => {
+    setClicks(clicks + 1);
+  };
 
   return (
     <View style={styles.container}>
-      <Text>Вы кликнули {count} раз(а)</Text>
-      <TouchableOpacity onPress={() => setCount(count + 1)}>
-        <Text style={{ marginTop: 10 }}>Нажми на меня</Text>
+      <Text style={styles.counter}>Количество нажатий: {clicks}</Text>
+      <TouchableOpacity style={styles.button} onPress={incrementClicks}>
+        <Text style={styles.buttonText}>Нажми на меня</Text>
       </TouchableOpacity>
     </View>
   );
 };
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5F5FF',
   },
-  buttonContainer: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    marginBottom: 30,
+  counter: {
+    fontSize: 16,
+    backgroundColor: '#E0E0E0',
+    padding: 10,
+    borderRadius: 5,
+    color: '#000',
+    marginBottom: 20,
+    textAlign: 'center',
+    width: '80%',
+  },
+  button: {
+    backgroundColor: '#D7E3FC',
+    paddingVertical: 15,
+    paddingHorizontal: 20,
+    borderRadius: 5,
+    width: '80%',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#0000FF',
+    fontWeight: 'bold',
+    fontSize: 16,
   },
 });
 
