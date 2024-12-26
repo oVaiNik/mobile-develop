39a517e830925be77ae0ad58f3b30138ddb4e4c4
AG1CE <144991640+AG1CE@users.noreply.github.com>
2024-12-26 20:30:50 +0900

    Icons

--- gavrilevAysien/App.js
+++ gavrilevAysien/App.js
@@ -5,6 +5,7 @@ import Lab1 from "./screens/lab1";
 import Lab2 from "./screens/lab2";
 import Lab3 from "./screens/lab3";
 import { NavigationContainer } from "@react-navigation/native";
+import { Image } from "react-native";
 const colors = ["black", "red", "blue"];
 
 const Tab = createBottomTabNavigator();
@@ -16,42 +17,54 @@ export default function App() {
           name="Lab1"
           component={Lab1}
           options={{
-            tabBarIcon: () => null,
-            tabBarLabel: 'Lab1',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("./assets/icons/lab1.png")}
+              />
+            ),
+            tabBarLabel: "Lab1",
             tabBarLabelStyle: {
-              fontSize: 10, 
-              fontWeight: 'bold', 
-              color: 'black', 
-              textAlign: 'center',
-            } 
+              fontSize: 10,
+              fontWeight: "bold",
+              color: "black",
+              textAlign: "center",
+            },
           }}
         />
         <Tab.Screen
           name="Lab2"
           component={Lab2}
           options={{
-            tabBarIcon: () => null,
-            tabBarLabel: 'Lab2',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("./assets/icons/lab2.png")}
+              />
+            ),
+            tabBarLabel: "Lab2",
             tabBarLabelStyle: {
-              fontSize: 10, 
-              fontWeight: 'bold', 
-              color: 'black', 
-              textAlign: 'center',
-            } 
+              fontSize: 10,
+              fontWeight: "bold",
+              color: "black",
+              textAlign: "center",
+            },
           }}
         />
         <Tab.Screen
           name="Lab3"
           component={Lab3}
           options={{
-            tabBarIcon: () => null,
-            tabBarLabel: 'Lab3',
+            tabBarIcon: ({ focused }) => (
+              <Image
+                source={require("./assets/icons/lab3.png")}
+              />
+            ),
+            tabBarLabel: "Lab3",
             tabBarLabelStyle: {
-              fontSize: 10, 
-              fontWeight: 'bold', 
-              color: 'black', 
-              textAlign: 'center',
-            } 
+              fontSize: 10,
+              fontWeight: "bold",
+              color: "black",
+              textAlign: "center",
+            },
           }}
         />
       </Tab.Navigator>
