9b9e922eac3c2450fbf6e2dcec46012df5c3c89f
oVaiNik <shadowvova2299@gmail.com>
2024-12-28 02:28:46 +0900

    Лабораторная работа 6

--- afanasevVladimir/App.js
+++ afanasevVladimir/App.js
@@ -5,12 +5,12 @@ import { ThemeProvider, useTheme } from "./ThemeContext.js";
 import Lab1 from "./layouts/lab1";
 import Lab2 from "./layouts/lab2-3";
 import Lab4 from "./layouts/lab4";
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const Tab = createBottomTabNavigator();
 
 const AppTabs = () => {
   const { isDarkTheme } = useTheme();
-
   return (
     <Tab.Navigator
       screenOptions={{
@@ -21,9 +21,36 @@ const AppTabs = () => {
         tabBarInactiveTintColor: isDarkTheme ? "#aaa" : "#555",
       }}
     >
-      <Tab.Screen name="Lab1" component={Lab1} />
-      <Tab.Screen name="Lab2" component={Lab2} />
-      <Tab.Screen name="Lab4" component={Lab4} />
+      <Tab.Screen
+        name="Lab1"
+        component={Lab1}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="color-lens" color={color} size={size} />
+          ),
+          tabBarLabel: () => null, // Устанавливаем tabBarLabel в null
+        }}
+      />
+      <Tab.Screen
+        name="Lab2"
+        component={Lab2}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name="cloud" color={color} size={size} />
+          ),
+          tabBarLabel: () => null, // Устанавливаем tabBarLabel в null
+        }}
+      />
+      <Tab.Screen
+        name="Lab4"
+        component={Lab4}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <Icon name={isDarkTheme ? "switch-right" : "switch-left"} color={color} size={size} />
+          ),
+          tabBarLabel: () => null, // Устанавливаем tabBarLabel в null
+        }}
+      />
     </Tab.Navigator>
   );
 };
