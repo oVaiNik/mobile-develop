bd34236a51a5bfa0f6da7b661c6da21186ea7eda
oVaiNik <shadowvova2299@gmail.com>
2024-12-27 20:19:33 +0900

    Сделал Lab 2

--- afanasevVladimir/App.js
+++ afanasevVladimir/App.js
@@ -1,31 +1,28 @@
 import React from "react";
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { StyleSheet, View } from 'react-native';
-
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Lab1 from "./layouts/lab1";
 import Lab2 from "./layouts/lab2-3";
 
 const Tab = createBottomTabNavigator();
 
 const AppTabs = () => {
+  const { isDarkTheme } = useTheme();
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Lab1" component={Lab1} />
-        <Tab.Screen name="Lab2" component={Lab2} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Lab1" component={Lab1} />
+      <Tab.Screen name="Lab2" component={Lab2} />
+    </Tab.Navigator>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-
-export default AppTabs;
+export default function App() {
+  return (
+    <ThemeProvider>
+      <NavigationContainer>
+        <AppTabs />
+      </NavigationContainer>
+    </ThemeProvider>
+  );
+}
